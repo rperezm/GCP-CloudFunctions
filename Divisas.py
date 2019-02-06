0fd59111fd1a440b2c50c0696882800bdf315399
rperez5 <rperez@option.cl>
2019-02-06 09:48:56 -0300

    Add Streaming insert into Bq

--- Divisas.py
+++ Divisas.py
@@ -1,7 +1,7 @@
 from google.cloud import bigquery
 import logging
 import requests
-
+import os
 
 def get_divisas(request):
     request_json = request.get_json(silent=True)
@@ -9,25 +9,24 @@ def get_divisas(request):
 
     '''
     https://api.sbif.cl/documentacion/Dolar.html
-
     '''
-    if started == 'begin':
-        bigquery_client = bigquery.Client()
-        # Get Dolar Price
-        dataset_ref = bigquery_client.dataset('misc')
-        table_ref = dataset_ref.table('Dolar')
-        table = bigquery_client.get_table(table_ref)
+    bigquery_client = bigquery.Client()
+    
+    dataset_ref = bigquery_client.dataset('misc')
+    table_ref = dataset_ref.table('Dolar')
+    table = bigquery_client.get_table(table_ref)
 
-        url_api = os.environ.get("url_api")
-        key = os.environ.get("key_api")
-        output_format = "json"
+    url_api = os.environ.get("url_api")
+    key = os.environ.get("key_api")
+    output_format = "json"
 
-        url = url.api.format(url_api, output_format)
-
-        r = requests.get(url)
-        for item in r.json()["Dolares"]:
-            rows_to_insert = [(item["Fecha"], item["Valor"])]
-            bigquery_client.insert_rows(table, rows_to_insert)
-
-            logging.warn('Fecha :' + item["Fecha"] + ' Valor :' + item["Valor"] + ' Inserted !!!')
-
+    url = url_api.format(key, output_format)
+    r = requests.get(url)
+    
+    for item in r.json()["Dolares"]:
+        rows_to_insert = [(u"{0}".format(item["Fecha"]), item["Valor"].replace(",","."))]
+        errors = bigquery_client.insert_rows(table, rows_to_insert)
+        assert errors == []
+        msg='Fecha :' + item["Fecha"] + ' Valor :' + item["Valor"] + ' Inserted !!!'
+        logging.warn(msg)
+    return msg 
